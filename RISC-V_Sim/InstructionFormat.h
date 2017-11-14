5f91fb68b15820392a67dab00fa770ee86f0e30a
TheAIBot <andreasuranus@hotmail.com>
2017-11-14 01:52:02 +0100

    added static asserts to verify the length of the instruction formats

--- RISC-V_Sim/InstructionFormat.h
+++ RISC-V_Sim/InstructionFormat.h
@@ -5,10 +5,10 @@
 struct RType
 {
 	uint32_t opcode : 7;
-	uint32_t rd : 5;
+	uint32_t rd     : 5;
 	uint32_t funct3 : 3;
-	uint32_t rs1 : 5;
-	uint32_t rs2 : 5;
+	uint32_t rs1    : 5;
+	uint32_t rs2    : 5;
 	uint32_t funct7 : 7;
 };
 union URType
@@ -16,13 +16,14 @@ union URType
 	uint32_t rawInstruction;
 	RType type;
 };
+static_assert(sizeof(RType) == sizeof(uint32_t), "RType isn't 4 bytes long");
 
 struct IType
 {
-	uint32_t opcode : 7;
-	uint32_t rd : 5;
-	uint32_t funct3 : 3;
-	uint32_t rs1 : 5;
+	uint32_t opcode    :  7;
+	uint32_t rd        :  5;
+	uint32_t funct3    :  3;
+	uint32_t rs1       :  5;
 	uint32_t immediate : 12;
 };
 union UIType
@@ -30,14 +31,15 @@ union UIType
 	uint32_t rawInstruction;
 	IType type;
 };
+static_assert(sizeof(IType) == sizeof(uint32_t), "IType isn't 4 bytes long");
 
 struct SType
 {
-	uint32_t opcode : 7;
+	uint32_t opcode     : 7;
 	uint32_t immediate1 : 5;
-	uint32_t funct3 : 3;
-	uint32_t rs1 : 5;
-	uint32_t rs2 : 5;
+	uint32_t funct3     : 3;
+	uint32_t rs1        : 5;
+	uint32_t rs2        : 5;
 	uint32_t immediate2 : 7;
 };
 union USType
@@ -45,15 +47,16 @@ union USType
 	uint32_t rawInstruction;
 	SType type;
 };
+static_assert(sizeof(SType) == sizeof(uint32_t), "SType isn't 4 bytes long");
 
 struct SBType
 {
-	uint32_t opcode : 7;
+	uint32_t opcode     : 7;
 	uint32_t immediate1 : 1;
 	uint32_t immediate2 : 4;
-	uint32_t funct3 : 3;
-	uint32_t rs1 : 5;
-	uint32_t rs2 : 5;
+	uint32_t funct3     : 3;
+	uint32_t rs1        : 5;
+	uint32_t rs2        : 5;
 	uint32_t immediate3 : 6;
 	uint32_t immediate4 : 1;
 };
@@ -62,11 +65,12 @@ union USBType
 	uint32_t rawInstruction;
 	SBType type;
 };
+static_assert(sizeof(SBType) == sizeof(uint32_t), "SBType isn't 4 bytes long");
 
 struct UJType
 {
-	uint32_t opcode : 7;
-	uint32_t rd : 5;
+	uint32_t opcode     : 7;
+	uint32_t rd         : 5;
 	uint32_t immediate1 : 8;
 	uint32_t immediate2 : 1;
 	uint32_t immediate3 : 10;
@@ -77,11 +81,12 @@ union UUJType
 	uint32_t rawInstruction;
 	UJType type;
 };
+static_assert(sizeof(UJType) == sizeof(uint32_t), "UJType isn't 4 bytes long");
 
 struct UType
 {
-	uint32_t opcode : 7;
-	uint32_t rd : 5;
+	uint32_t opcode    :  7;
+	uint32_t rd        :  5;
 	uint32_t immediate : 20;
 };
 union UUType
@@ -89,3 +94,4 @@ union UUType
 	uint32_t rawInstruction;
 	UType type;
 };
+static_assert(sizeof(UType) == sizeof(uint32_t), "UType isn't 4 bytes long");
